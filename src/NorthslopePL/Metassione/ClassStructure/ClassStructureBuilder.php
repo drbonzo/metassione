daa0e6f6afe0c0e022199756abe902b90d919234
dr_bonzo <dr_bonzo@northslope.pl>
2014-10-30 13:43:25 +0100

    ClassStructureBuilder

--- src/NorthslopePL/Metassione/ClassStructure/ClassStructureBuilder.php
+++ src/NorthslopePL/Metassione/ClassStructure/ClassStructureBuilder.php
@@ -1,17 +1,37 @@
 <?php
 namespace NorthslopePL\Metassione\ClassStructure;
 
+use NorthslopePL\Metassione\Metadata\MetadataHelper;
+
 class ClassStructureBuilder
 {
 	/**
 	 * @param string $classname
+	 *
 	 * @return ClassStructure
+	 *
+	 * @throws ClassStructureException if class is not found
 	 */
 	public function buildClassStructure($classname)
 	{
 		$classStructure = new ClassStructure();
+		$metadataHelper = new MetadataHelper();
+
+		if (!class_exists($classname))
+		{
+			throw new ClassStructureException(sprintf("Class not found: '%s'", $classname));
+		}
+
 		$classStructure->setClassname($classname);
-		
+
+		$propertyStructureBuilder = new PropertyStructureBuilder();
+		$reflectionProperties = $metadataHelper->getPropertyReflectionsFromReflectionClassOrItsParentClasses(new \ReflectionClass($classname));
+		foreach ($reflectionProperties as $reflectionProperty)
+		{
+			$propertyStructure = $propertyStructureBuilder->buildPropertyStructure($reflectionProperty);
+			$classStructure->addPropertyStructure($propertyStructure);
+		}
+
 		return $classStructure;
 	}
 }
--- tests/Tests/NorthslopePL/Metassione/ClassStructure/ClassStructureBuilderTest.php
+++ tests/Tests/NorthslopePL/Metassione/ClassStructure/ClassStructureBuilderTest.php
@@ -2,7 +2,10 @@
 namespace Tests\NorthslopePL\Metassione\ClassStructure;
 
 use NorthslopePL\Metassione\ClassStructure\ClassStructureBuilder;
+use NorthslopePL\Metassione\ClassStructure\PropertyStructure;
+use Tests\NorthslopePL\Metassione\ExampleClasses\ChildKlass;
 use Tests\NorthslopePL\Metassione\ExampleClasses\EmptyKlass;
+use Tests\NorthslopePL\Metassione\ExampleClasses\SimpleKlass;
 
 class ClassStructureBuilderTest extends \PHPUnit_Framework_TestCase
 {
@@ -25,11 +28,31 @@ class ClassStructureBuilderTest extends \PHPUnit_Framework_TestCase
 
 	public function testBuildingForFlatKlass()
 	{
+		$classStructure = $this->classStructureBuilder->buildClassStructure(SimpleKlass::class);
+		$this->assertCount(3, $classStructure->getPropertyStructures());
+		$this->assertEquals(SimpleKlass::class, $classStructure->getClassname());
 
+		$this->assertInstanceOf(PropertyStructure::class, $classStructure->getPropertyStructure('count'));
+		$this->assertInstanceOf(PropertyStructure::class, $classStructure->getPropertyStructure('name'));
+		$this->assertInstanceOf(PropertyStructure::class, $classStructure->getPropertyStructure('value'));
 	}
 
 	public function testBuildingForClassWithDeepInheritance()
 	{
+		$classStructure = $this->classStructureBuilder->buildClassStructure(ChildKlass::class);
+		$this->assertCount(4, $classStructure->getPropertyStructures());
+		$this->assertEquals(ChildKlass::class, $classStructure->getClassname());
 
+		$this->assertInstanceOf(PropertyStructure::class, $classStructure->getPropertyStructure('childProperty'));
+		$this->assertInstanceOf(PropertyStructure::class, $classStructure->getPropertyStructure('parentProperty'));
+		$this->assertInstanceOf(PropertyStructure::class, $classStructure->getPropertyStructure('grandparentProperty'));
+		$this->assertInstanceOf(PropertyStructure::class, $classStructure->getPropertyStructure('grandparentProtectedProperty'));
+	}
+
+	public function testBuildingForNonexistingClassname()
+	{
+		$nonexistingClassname = 'Foo\Bar\ThisClassDoesNotExist';
+		$this->setExpectedException('NorthslopePL\Metassione\ClassStructure\ClassStructureException', "Class not found: 'Foo\\Bar\\ThisClassDoesNotExist'");
+		$this->classStructureBuilder->buildClassStructure($nonexistingClassname);
 	}
 }
