403db3dfdb75f01e624fb4b386d1b8fb9c6a8605
dr_bonzo <dr_bonzo@northslope.pl>
2016-04-21 21:07:07 +0200

    ClassPropertyFinder

---
+++ tests/NorthslopePL/Metassione2/Tests/Fixtures/Klasses/BasicTypesKlass.php
@@ -0,0 +1,56 @@
+<?php
+namespace NorthslopePL\Metassione2\Tests\Fixtures\Klasses;
+
+class BasicTypesKlass
+{
+	/**
+	 * @var string
+	 */
+	public $stringValue;
+
+	/**
+	 * @var integer
+	 */
+	public $integerValue;
+
+	/**
+	 * @var int
+	 */
+	public $intValue;
+
+	/**
+	 * @var float
+	 */
+	public $floatValue;
+
+	/**
+	 * @var double
+	 */
+	public $doubleValue;
+
+	/**
+	 * @var boolean
+	 */
+	public $booleanValue;
+
+	/**
+	 * @var bool
+	 */
+	public $boolValue;
+
+	/**
+	 * @var void
+	 */
+	public $voidValue;
+
+	/**
+	 * @var mixed
+	 */
+	public $mixedValue;
+
+	/**
+	 * @var null
+	 */
+	public $nullValue;
+
+}
---
+++ tests/NorthslopePL/Metassione2/Tests/Fixtures/Klasses/BasicTypesWithNullsKlass.php
@@ -0,0 +1,56 @@
+<?php
+namespace NorthslopePL\Metassione2\Tests\Fixtures\Klasses;
+
+class BasicTypesWithNullsKlass
+{
+	/**
+	 * @var string|null
+	 */
+	public $stringValue;
+
+	/**
+	 * @var integer|null
+	 */
+	public $integerValue;
+
+	/**
+	 * @var int|null
+	 */
+	public $intValue;
+
+	/**
+	 * @var float|null
+	 */
+	public $floatValue;
+
+	/**
+	 * @var double|null
+	 */
+	public $doubleValue;
+
+	/**
+	 * @var boolean|null
+	 */
+	public $booleanValue;
+
+	/**
+	 * @var bool|null
+	 */
+	public $boolValue;
+
+	/**
+	 * @var void|null
+	 */
+	public $voidValue;
+
+	/**
+	 * @var mixed|null
+	 */
+	public $mixedValue;
+
+	/**
+	 * @var null|null
+	 */
+	public $nullValue;
+
+}
